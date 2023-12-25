678b985bdda06704803781538d30f12a6fa013c7
Katarios2015 <prichal7@bk.ru>
2023-12-25 17:53:51 +0300

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-import {createPhotoDescriptions} from './data.js';
+//import {createPhotoDescriptions} from './data.js';
+import {showAlert} from './util.js';
+import {getData} from './fetch.js';
 import {createGallery} from './mini-pictures.js';
 
 import {initFullPhotoOpenHandler} from './full-picture.js';
-import {initSubmitUploadformHandler} from './upload-photo-form.js';
+import {initSubmitUploadformHandler, closePhotoEditor} from './upload-photo-form.js';
 
-const photoDescriptions = createPhotoDescriptions();
-
-createGallery(photoDescriptions);
-initFullPhotoOpenHandler(photoDescriptions);
-initSubmitUploadformHandler();
+//const photoDescriptions = createPhotoDescriptions();
+getData()
+  .then((photos) => {
+    createGallery(photos);
+    initFullPhotoOpenHandler(photos);
+  })
+  .catch(
+    (err) => {
+      showAlert(err.message);
+    }
+  );
+initSubmitUploadformHandler(closePhotoEditor);
